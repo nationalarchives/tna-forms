77caffb7110617c4ef6e1b6bab53328c05fb1ec2
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-09-22 11:20:51 +0100

    Added basic files

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdiaconita
+ * Date: 22/09/2016
+ * Time: 11:18
+ */
---
+++ tna-forms.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: TNA Forms
+ * Plugin URI: https://github.com/nationalarchives/tna-forms
+ * Description: The National Archives form validations plugin.
+ * Version: 0.1
+ * Author: Mihai Diaconita
+ * Author URI: https://github.com/nationalarchives
+ * License: GPL2
+ */
