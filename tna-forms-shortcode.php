1c455d3de4e138946c306d5c2e0bad20d3ca465f
Chris Bishop <domingobishop@gmail.com>
2016-10-19 13:58:50 +0100

    css temp !important

--- tna-forms-shortcode.php
+++ tna-forms-shortcode.php
@@ -5,17 +5,18 @@
  */
 
 function tna_forms_shordcode( $atts ) {
+
 	$a = shortcode_atts( array(
-		'id' => 'form'
+		'name' => 'form'
 	), $atts );
-	switch ( $a['id'] ) {
-		case 'bitish-citizenship':
+
+	switch ( $a['name'] ) {
+		case 'British citizenship':
 			return return_form_british_citizenship();
 			break;
 		default:
 			return 'No form defined';
 	}
-
 }
 
 add_shortcode( 'tna-form', 'tna_forms_shordcode' );
