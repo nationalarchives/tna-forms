8363661b8536959cf665bc312d4a99f445b024d5
Chris Bishop <domingobishop@gmail.com>
2016-11-18 15:14:00 +0000

    method exists tests

--- tests/FormsFunctionsTest.php
+++ tests/FormsFunctionsTest.php
@@ -102,4 +102,13 @@ class FormsFunctionsTest extends \PHPUnit_Framework_TestCase {
 	{
 		$this->assertTrue(function_exists('get_tna_email'));
 	}
+	public function testFormBuilder()
+	{
+		$this->assertTrue( class_exists('Form_Builder') );
+	}
+	public function testFormBuilderMethodFormBegins()
+	{
+		$myClass = new \Form_Builder();
+		$this->assertTrue( method_exists($myClass, 'form_begins') );
+	}
 }
