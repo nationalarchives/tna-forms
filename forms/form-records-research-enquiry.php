9a6a9538eca17355e5c8fe081d053c02862bb7cf
Chris Bishop <domingobishop@gmail.com>
2016-11-21 15:50:35 +0000

    removed token condition

--- forms/form-records-research-enquiry.php
+++ forms/form-records-research-enquiry.php
@@ -50,12 +50,12 @@ function process_form_rre() {
 
 		// Checks for token
 		// If the token exists then the form has been submitted so do nothing
-		$token = filter_input( INPUT_POST, 'token' );
+		/* $token = filter_input( INPUT_POST, 'token' );
 		if ( get_transient( 'token_' . $token ) ) {
 			$_POST = array();
 			return;
 		}
-		set_transient( 'token_' . $token, 'form-token', 360 );
+		set_transient( 'token_' . $token, 'form-token', 360 ); */
 
 		// Global variables
 		global $tna_success_message,
