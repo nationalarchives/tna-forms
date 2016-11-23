16c735e1e0893de9f855920c10c3146109b5f20f
Chris Bishop <domingobishop@gmail.com>
2016-11-23 09:24:59 +0000

    form builder tests

--- tests/FormBuilderTest.php
+++ tests/FormBuilderTest.php
@@ -67,25 +67,55 @@ class FormBuilderTest extends \PHPUnit_Framework_TestCase {
 		$class = new \Form_Builder();
 		$this->assertTrue( method_exists($class, 'form_textarea_input') );
 	}
+	public function testFormBuilderMethodFormTextareaInputReturn()
+	{
+		$class = new \Form_Builder();
+		$html = $class->form_textarea_input( 'Label', 'id', 'name', 'Error message', 'Hint text');
+		$this->assertEquals($html, '<div class="form-row"><label for="id">Label</label><p class="form-hint">Hint text</p><textarea id="id" name="name"  aria-required="true" required ></textarea></div>');
+	}
 	public function testFormBuilderMethodFormEmailInput()
 	{
 		$class = new \Form_Builder();
 		$this->assertTrue( method_exists($class, 'form_email_input') );
+	}
+	public function testFormBuilderMethodFormEmailInputReturns()
+	{
+		$class = new \Form_Builder();
+		$html = $class->form_email_input( 'Label', 'id', 'name', 'Error message' );
+		$this->assertEquals($html, '<div class="form-row"><label for="id">Label</label><input type="email" id="id" name="name"  aria-required="true" required ></div>');
 	}
 	public function testFormBuilderMethodFormCheckboxInput()
 	{
 		$class = new \Form_Builder();
 		$this->assertTrue( method_exists($class, 'form_checkbox_input') );
 	}
+	public function testFormBuilderMethodFormCheckboxInputReturns()
+	{
+		$class = new \Form_Builder();
+		$html = $class->form_checkbox_input( 'Label', 'id', 'name' );
+		$this->assertEquals($html, '<div class="form-row checkbox"><input type="checkbox" id="id" name="name" value="Yes" ><label for="id">Label</label></div>');
+	}
 	public function testFormBuilderMethodSubmitForm()
 	{
 		$class = new \Form_Builder();
 		$this->assertTrue( method_exists($class, 'submit_form') );
 	}
+	public function testFormBuilderMethodSubmitFormReturns()
+	{
+		$class = new \Form_Builder();
+		$html = $class->submit_form( 'submit', 'submit' );
+		$this->assertEquals($html, '<div class="form-row"><input type="submit" name="submit" id="submit" value="Submit" class="button"></div>');
+	}
 	public function testFormBuilderMethodHelpText()
 	{
 		$class = new \Form_Builder();
 		$this->assertTrue( method_exists($class, 'help_text') );
+	}
+	public function testFormBuilderMethodHelpTextReturns()
+	{
+		$class = new \Form_Builder();
+		$html = $class->help_text( 'Help text' );
+		$this->assertEquals($html, '<div class="form-row"><p>Help text</p></div>');
 	}
 	public function testFormBuilderMethodRequiredAtts()
 	{
