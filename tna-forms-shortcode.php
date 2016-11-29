4e735f63afb5afb84dad8856ab7320fda1f71f06
Chris Bishop <domingobishop@gmail.com>
2016-11-29 11:46:49 +0000

    form-builder shortcode

--- tna-forms-shortcode.php
+++ tna-forms-shortcode.php
@@ -33,3 +33,43 @@ function tna_forms_shortcode( $atts ) {
 }
 
 add_shortcode( 'tna-form', 'tna_forms_shortcode' );
+
+function tnafb_shortcode( $atts ) {
+
+	$a = shortcode_atts( array(
+		'type' => 'text',
+		'name' => 'Full name',
+		'error' => 'Please enter your full name',
+		'hint'  => ''
+	), $atts );
+
+	$html = new Form_Builder;
+
+	switch ( $a['type'] ) {
+		case 'start':
+			$id = strtolower( str_replace(' ', '_', $a['name']) );
+			$value = strtolower( str_replace(' ', '-', $a['name']) );
+			return $html->form_begins( $id, $value ) .
+			       $html->fieldset_begins( $a['name'] );
+			break;
+		case 'end':
+			$id = strtolower( str_replace(' ', '_', $a['name']) );
+			$name = strtolower( str_replace(' ', '-', $a['name']) );
+			return $html->submit_form( $name, $id ) .
+			       $html->fieldset_ends() .
+			       $html->form_ends();
+			break;
+		case 'text':
+			$id = strtolower( str_replace(' ', '_', $a['name']) );
+			$name = strtolower( str_replace(' ', '-', $a['name']) );
+			return $html->form_text_input( $a['name'], $id, $name, $a['error'], $a['hint'] );
+			break;
+		case 'textarea':
+			$id = strtolower( str_replace(' ', '_', $a['name']) );
+			$name = strtolower( str_replace(' ', '-', $a['name']) );
+			return $html->form_textarea_input( $a['name'], $id, $name, $a['error'], $a['hint'] );
+			break;
+	}
+}
+
+add_shortcode( 'form-builder', 'tnafb_shortcode' );
