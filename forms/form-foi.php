83e7dad6d89cd232ed4d8b7a852a103d2d39d646
Chris Bishop <domingobishop@gmail.com>
2017-01-18 11:00:21 +0000

    formatting

--- forms/form-foi.php
+++ forms/form-foi.php
@@ -9,10 +9,10 @@ function return_form_foi() {
 	// HTML form string
 	$html = new Form_Builder;
 	$form =  $html->form_foi_begins( 'https://test.nationalarchives.gov.uk/contact/contactform.asp', 'contactForm', 'contactForm' ) .
-	         $html->form_hidden_input( 'formID', '10') .
-	         $html->form_hidden_input( 'formType', '1') .
-	         $html->form_hidden_input( 'formTitle', 'Freedom of Information enquiry') .
-	         $html->form_hidden_input( 'formResponseTarget', '20 working days') .
+	         $html->form_hidden_input( 'formID', '10' ) .
+	         $html->form_hidden_input( 'formType', '1' ) .
+	         $html->form_hidden_input( 'formTitle', 'Freedom of Information enquiry' ) .
+	         $html->form_hidden_input( 'formResponseTarget', '20 working days' ) .
 	         $html->fieldset_begins( 'Your enquiry' ) .
 	         $html->form_text_input( 'Title', 'title', 'title' ) .
 	         $html->form_text_input( 'First name', 'forename', 'forename' ) .
