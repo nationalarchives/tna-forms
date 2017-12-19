673e9f7fa664c8d79ec70be8f658f5424b088ecb
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2017-12-19 15:42:41 +0000

    Add client side validation to Apply For Traning form

--- js/compiled/tna-forms-compiled.js
+++ js/compiled/tna-forms-compiled.js
@@ -1054,11 +1054,11 @@ function iacsTrainingForm(){
  * 3. Add the validation rules
  * */
 
-function applyToFilmForm(){
+function applyForTrainingForm(){
     /**
      * 1. Declare variables
      * */
-    var formName = "#apply-to-film";
+    var formName = "#apply-for-training";
     var form = $(formName);
 
     /**
@@ -1066,6 +1066,29 @@ function applyToFilmForm(){
      * */
     formMethods();
 
+    // Declare self executing function and keep all my variables inside this scope
+    (function(){
+
+        // Declare variables
+        var $firstSession = $('#session_first_choice');
+        var $secondSession = $('#session_second_choice');
+
+        // Declare anonymous function
+        var compareSessions = function (sessionOne, sessionTwo){
+            sessionOne.change(function() {
+                sessionTwo.find('option').prop("disabled", false);
+                var selectedItem = $(this).val();
+                if (selectedItem) {
+                    sessionTwo.find('option[value="' + selectedItem + '"]').prop("disabled", true);
+                }
+            });
+        };
+
+        // Compare session 1 with 2
+        compareSessions($firstSession,$secondSession);
+        // Compare session 2 with 1
+        compareSessions($secondSession,$firstSession);
+    })();
     /**
      * 3. Add the validation rules
      * */
@@ -1094,12 +1117,24 @@ function applyToFilmForm(){
             "confirm-email-required": {
                 equalTo: "#email"
             },
-            "date-required":{
-                required:true,
+            "job-title-required":{
+                required: true,
                 noSpace: true
             },
-            "policy-required":{
-                required:true,
+            "department-agency-organisation-required":{
+                required: true,
+                noSpace: true
+            },
+            "postal-required":{
+                required: true,
+                noSpace: true
+            },
+            "session-first-choice-required":{
+                required: true,
+                noSpace: true
+            },
+            "session-second-choice-required":{
+                required: true,
                 noSpace: true
             }
 
@@ -1116,16 +1151,25 @@ function applyToFilmForm(){
                 required:"Please enter your email address",
                 equalTo: "Please enter your email address again"
             },
-            "date-required":{
-                required:"Please enter your filming date"
+            "job-title-required":{
+                required: "Please enter your job title"
             },
-            "policy-required":{
-                required:"Please agree to the terms and conditions"
+            "department-agency-organisation-required":{
+                required: "Please enter your department, agency or organisation"
+            },
+            "postal-required":{
+                required: "Please enter your postal address"
+            },
+            "session-first-choice-required":{
+                required: "Please select an option"
+            },
+            "session-second-choice-required":{
+                required: "Please select an option"
             }
         }
     });
 
-    $("input[name='submit-atf']").on('click', function(){
+    $("input[name='submit-apply-for-training']").on('click', function(){
         var emphAlert = ($('.emphasis-block.error-message').length === 1);
         if(form.valid() !== true) {
             if(emphAlert) {
@@ -1566,141 +1610,6 @@ function dcfForm(){
     });
 
 
-};/**
- * @contact-form-name: Apply to film
- *
- * ----- Table of contents -------------------------------------
- *
- * 1. Define variables
- * 2. Include custom form methods from methods.js
- * 3. Add the validation rules
- * */
-
-function applyForTrainingForm(){
-    /**
-     * 1. Declare variables
-     * */
-    var formName = "#apply-for-training";
-    var form = $(formName);
-
-    /**
-     * 2. Included custom form validation methods from methods.js
-     * */
-    formMethods();
-
-    // Declare self executing function and keep all my variables inside this scope
-    (function(){
-
-        // Declare variables
-        var $firstSession = $('#session_first_choice');
-        var $secondSession = $('#session_second_choice');
-
-        // Declare anonymous function
-        var compareSessions = function (sessionOne, sessionTwo){
-            sessionOne.change(function() {
-                sessionTwo.find('option').prop("disabled", false);
-                var selectedItem = $(this).val();
-                if (selectedItem) {
-                    sessionTwo.find('option[value="' + selectedItem + '"]').prop("disabled", true);
-                }
-            });
-        };
-
-        // Compare session 1 with 2
-        compareSessions($firstSession,$secondSession);
-        // Compare session 2 with 1
-        compareSessions($secondSession,$firstSession);
-    })();
-    /**
-     * 3. Add the validation rules
-     * */
-    form.validate({
-        errorElement: 'span',
-        errorClass: 'form-error form-hint',
-        highlight: function(element, errorClass, validClass) {
-            $(element).closest('input[type="text"]').addClass("form-warning");
-            $(element).closest('input[type="email"]').addClass("form-warning");
-            $(element).closest('textarea').addClass("form-warning");
-        },
-        unhighlight: function(element, errorClass, validClass) {
-            $(element).closest('input[type="text"]').removeClass("form-warning");
-            $(element).closest('input[type="email"]').removeClass("form-warning");
-            $(element).closest('textarea').removeClass("form-warning");
-        },
-        rules: {
-            "full-name-required": {
-                required: true,
-                noSpace: true
-            },
-            "email-required": {
-                required: true,
-                email:true
-            },
-            "confirm-email-required": {
-                equalTo: "#email"
-            },
-            "job-title-required":{
-                required: true,
-                noSpace: true
-            },
-            "department-agency-organisation-required":{
-                required: true,
-                noSpace: true
-            },
-            "postal-required":{
-                required: true,
-                noSpace: true
-            },
-            "session-first-choice-required":{
-                required: true,
-                noSpace: true
-            },
-            "session-second-choice-required":{
-                required: true,
-                noSpace: true
-            }
-
-        },
-        /**
-         * Error messages
-         * */
-        messages: {
-            "full-name-required": {
-                required: "Please enter your full name"
-            },
-            "email-required": "Please enter your email address",
-            "confirm-email-required": {
-                required:"Please enter your email address",
-                equalTo: "Please enter your email address again"
-            },
-            "job-title-required":{
-                required: "Please enter your job title"
-            },
-            "department-agency-organisation-required":{
-                required: "Please enter your department, agency or organisation"
-            },
-            "postal-required":{
-                required: "Please enter your postal address"
-            },
-            "session-first-choice-required":{
-                required: "Please select an option"
-            },
-            "session-second-choice-required":{
-                required: "Please select an option"
-            }
-        }
-    });
-
-    $("input[name='submit-apply-for-training']").on('click', function(){
-        var emphAlert = ($('.emphasis-block.error-message').length === 1);
-        if(form.valid() !== true) {
-            if(emphAlert) {
-                $('.emphasis-block.error-message').show();
-            } else {
-                $(form).before().prepend('<div class="emphasis-block error-message" role="alert"><p class="h3">Sorry, there was a problem</p><p>Please check the highlighted fields to proceed.</p></div>');
-            }
-        }
-    });
 };/**
  * @name  : Newsletter and Contact forms Wordpress plugin
  * @author: TNA WebTeam
