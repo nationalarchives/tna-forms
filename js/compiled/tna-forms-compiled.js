cc5a5b5d07a8b2f33f844ab04b83ba28ad520e8f
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2017-12-20 12:38:22 +0000

    Add Apply to film client side validation to the form and compile the whole JS

--- js/compiled/tna-forms-compiled.js
+++ js/compiled/tna-forms-compiled.js
@@ -1607,6 +1607,97 @@ function dcfForm(){
 
 
 };/**
+ * @contact-form-name: Apply to film
+ *
+ * ----- Table of contents -------------------------------------
+ *
+ * 1. Define variables
+ * 2. Include custom form methods from methods.js
+ * 3. Add the validation rules
+ * */
+
+function applyToFilmForm(){
+    /**
+     * 1. Declare variables
+     * */
+    var formName = "#apply-to-film";
+    var form = $(formName);
+
+    /**
+     * 2. Included custom form validation methods from methods.js
+     * */
+    formMethods();
+
+    /**
+     * 3. Add the validation rules
+     * */
+    form.validate({
+        errorElement: 'span',
+        errorClass: 'form-error form-hint',
+        highlight: function(element, errorClass, validClass) {
+            $(element).closest('input[type="text"]').addClass("form-warning");
+            $(element).closest('input[type="email"]').addClass("form-warning");
+            $(element).closest('textarea').addClass("form-warning");
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).closest('input[type="text"]').removeClass("form-warning");
+            $(element).closest('input[type="email"]').removeClass("form-warning");
+            $(element).closest('textarea').removeClass("form-warning");
+        },
+        rules: {
+            "full-name-required": {
+                required: true,
+                noSpace: true
+            },
+            "email-required": {
+                required: true,
+                email:true
+            },
+            "confirm-email-required": {
+                equalTo: "#email"
+            },
+            "date-required":{
+                required:true,
+                noSpace: true
+            },
+            "policy-required":{
+                required:true,
+                noSpace: true
+            }
+
+        },
+        /**
+         * Error messages
+         * */
+        messages: {
+            "full-name-required": {
+                required: "Please enter your full name"
+            },
+            "email-required": "Please enter your email address",
+            "confirm-email-required": {
+                required:"Please enter your email address",
+                equalTo: "Please enter your email address again"
+            },
+            "date-required":{
+                required:"Please enter your filming date"
+            },
+            "policy-required":{
+                required:"Please agree to the terms and conditions"
+            }
+        }
+    });
+
+    $("input[name='submit-atf']").on('click', function(){
+        var emphAlert = ($('.emphasis-block.error-message').length === 1);
+        if(form.valid() !== true) {
+            if(emphAlert) {
+                $('.emphasis-block.error-message').show();
+            } else {
+                $(form).before().prepend('<div class="emphasis-block error-message" role="alert"><p class="h3">Sorry, there was a problem</p><p>Please check the highlighted fields to proceed.</p></div>');
+            }
+        }
+    });
+};/**
  * @name  : Newsletter and Contact forms Wordpress plugin
  * @author: TNA WebTeam
  * @owner : The National Archives
