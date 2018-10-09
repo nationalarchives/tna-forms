c78f5ee7ab187c1a0b2acb3bfba9df2e9aed693e
netdesignr <michaiel.webdesigner@gmail.com>
2018-10-09 22:30:51 +0100

    Add client side validation

---
+++ js/forms/form-letters-of-no-evidence.js
@@ -0,0 +1,85 @@
+/**
+ * @contact-form-name: Letters of no evidence
+ *
+ * ----- Table of contents -------------------------------------
+ *
+ * 1. Define variables
+ * 2. Include custom form methods from methods.js
+ * 3. Add the validation rules
+ * */
+
+function lettersOfNoEvidenceForm(){
+    /**
+     * 1. Declare variables
+     * */
+    var formName = "#letters_of_no_evidence";
+    var form = $(formName);
+
+    /**
+     * 2. Included custom form validation methods from methods.js
+     * */
+    formMethods();
+
+    /**
+     * 3. Add the validation rules
+     * */
+    form.validate({
+        errorElement: 'span',
+        errorClass: 'form-error form-hint',
+        highlight: function(element, errorClass, validClass) {
+            $(element).closest('input[type="text"]').addClass("form-warning");
+            $(element).closest('input[type="email"]').addClass("form-warning");
+            $(element).closest('textarea').addClass("form-warning");
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).closest('input[type="text"]').removeClass("form-warning");
+            $(element).closest('input[type="email"]').removeClass("form-warning");
+            $(element).closest('textarea').removeClass("form-warning");
+        },
+        rules: {
+            "full-name-required": {
+                required: true,
+                noSpace: true
+            },
+            "email-required": {
+                required: true,
+                email:true
+            },
+            "confirm-email-required": {
+                equalTo: "#email"
+            },
+            "file-format-required":{
+                required: true,
+                noSpace: true
+            }
+
+        },
+        /**
+         * Error messages
+         * */
+        messages: {
+            "full-name-required": {
+                required:"Please enter your full name"
+            },
+            "email-required": "Please enter your email address",
+            "confirm-email-required": {
+                required:"Please enter your email address",
+                equalTo: "Please enter your email address again"
+            },
+            "file-format-required":{
+                required:"Please enter the file format"
+            }
+        }
+    });
+
+    $("input[name='submit-letters-of-no-evidence']").on('click', function(){
+        var emphAlert = ($('.emphasis-block.error-message').length === 1);
+        if(form.valid() !== true) {
+            if(emphAlert) {
+                $('.emphasis-block.error-message').show();
+            } else {
+                $(form).before().prepend('<div class="emphasis-block error-message" role="alert"><p class="h3">Sorry, there was a problem</p><p>Please check the highlighted fields to proceed.</p></div>');
+            }
+        }
+    });
+}
