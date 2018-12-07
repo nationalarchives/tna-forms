bc10661d6993ef53248f44453bd75cf51595a82a
NetDesignr <michaiel.webdesigner@gmail.com>
2018-12-07 11:52:54 +0000

    Send XML out

--- tna-forms-processor.php
+++ tna-forms-processor.php
@@ -138,7 +138,7 @@ class Form_Processor {
 	 * @param string $tna_recipient
 	 * @param string $alt_recipient
 	 */
-	public function process_data( $form_name, $form_data, $tna_recipient = '', $alt_recipient = '' ) {
+	public function process_data( $form_name, $form_data, $tna_recipient = '', $alt_recipient = '', $send_xml_format = false ) {
 
 		// Global variables
 		global $tna_success_message,
@@ -186,7 +186,7 @@ class Form_Processor {
 
 			// Email to TNA
             if ($send_xml_format) {
-                $alt_email = $this->get_tna_email($alt_recipient);
+                //$alt_email = $this->get_tna_email($alt_recipient);
                 $form_content = $this->display_data($form_data) .
                     $form_content = "<br>" .
                         $form_content = "Cut and paste the XML appearing below this line into the 'Description' field in Infoservice." .
