a8c0e5ea618b5cf60674298a809264b2903f9724
grange85 <andy@grange85.co.uk>
2020-11-09 09:40:49 +0000

    fix validation

--- js/forms/form-freedom-of-information-enquiry.js
+++ js/forms/form-freedom-of-information-enquiry.js
@@ -37,6 +37,9 @@ function freedomOfInformationEnquiryForm(){
             $(element).closest('textarea').removeClass("form-warning");
         },
         rules: {
+            "title": {
+                noSpace: true
+            },
             "full-name-required": {
                 required: true,
                 noSpace: true
