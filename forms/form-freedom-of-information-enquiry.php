50f724a9ed030aa747c4531c08466e8092bd1697
grange85 <andy@grange85.co.uk>
2020-11-09 08:56:00 +0000

    add xml data processing

--- forms/form-freedom-of-information-enquiry.php
+++ forms/form-freedom-of-information-enquiry.php
@@ -29,7 +29,7 @@ function return_form_foie( $content ) {
 
 		$process = new Form_Processor;
 		$form_data = $process->get_data( $_POST );
-		$process->process_data( $form_name, $form_data, 'foiteam' );
+		$process->process_data( $form_name, $form_data, 'foiteam', '', '', true);
 	}
 
 	// HTML form string
